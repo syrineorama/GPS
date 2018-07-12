3b33b108f35f2238fb2c69331afae8f95ad9caf5
walid <bousnina.walid1@gmail.com>
2018-07-12 23:12:04 +0200

    authentification service

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,12 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new GpsApi\EspaceUserBundle\GpsApiEspaceUserBundle(),
             new GpsApi\AdministrationBundle\GpsApiAdministrationBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
