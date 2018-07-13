ebfe2dbe237d53a201419a8d2625e0db8eee4957
CYRINE HAMMEMI <CYRINE.HAMMEMI@IsetRades.com>
2018-07-13 11:27:06 +0100

    commit entities vehicule + boitier created

--- src/GpsApi/AdministrationBundle/Controller/BoitierController.php
+++ src/GpsApi/AdministrationBundle/Controller/BoitierController.php
@@ -9,12 +9,9 @@
 namespace GpsApi\AdministrationBundle\Controller;
 
 use GpsApi\AdministrationBundle\Entity\Boitier;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class BoitierController extends FOSRestController
 {
